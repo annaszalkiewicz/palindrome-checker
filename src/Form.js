071877792c6ffea93d50523a37654016461af70e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-14 20:21:53 +0100

    Add checkIfPalindrome method to Form component

--- src/Form.js
+++ src/Form.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.checkIfPalindrome = this.checkIfPalindrome.bind(this);
+
+  }
+  
+  checkIfPalindrome = (e) => {
+    e.preventDefault();
+    console.log('Submitted form');
+    
+  }
+
   render() {
     return (
       <section>
-        <form>
+        <form onSubmit={e => this.checkIfPalindrome(e)}>
           <label htmlFor="text">Type word, number or phrase</label>
           <input type="text" name="text" id="text"/>
           <button type="submit">Check for Palindrome</button>
