210362c905d712737e380540c9c984a4bd2195e9
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-20 23:51:22 +0100

    Create new component Animate

---
+++ src/Animate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Animate extends Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Animate;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Main from './Main';
+import Animate from './Animate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Animate />
         <header className="App-header">
           <h1>Is it palindrome?</h1>
           <h2>Palindrome Checker</h2>
