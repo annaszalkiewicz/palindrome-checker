a34a00f61786a08daf1611ef5bd042798da7e51f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-21 23:40:55 +0100

    Add createPhrase() and add array with palindromes

--- src/Animate.js
+++ src/Animate.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 class Animate extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: []
+    }
+  }
+
+  createPhrase = () => {
+
+    const array = ['anna', 'madam', '10201', 'Was it a cat I saw?', 'Do geese see God?', 'Race fast, safe car', 'Able was I ere I saw Elba', 'Rats live on no evil star', 'A man a plan a Canal Panama' ];
+  }
+  
+
   render() {
     return (
       <div></div>
