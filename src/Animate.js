ac728cb74ec356becea1250c276ee788837f78bd
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-23 23:25:43 +0100

    Add animation container and spans with palindrome

--- src/Animate.js
+++ src/Animate.js
@@ -7,25 +7,46 @@ class Animate extends Component {
     this.state = {
       words: []
     }
-    this.createPhrase = this.createPhrase.bind(this);
+    // this.createPhrase = this.createPhrase.bind(this);
+    // this.getRandom = this.getRandom.bind(this);
   }
 
-  componentDidMount = () => {
+  // componentDidMount = () => {
 
-    this.createPhrase();
+  //   this.getRandom();
 
-  }
+  // }
 
-  createPhrase = () => {
+  // createPhrase = () => {
 
-    const array = ['anna', 'madam', '10201', 'Was it a cat I saw?', 'Do geese see God?', 'Race fast, safe car', 'Able was I ere I saw Elba', 'Rats live on no evil star', 'A man a plan a Canal Panama' ];
+  //   const array = ['anna', 'madam', '10201', 'Was it a cat I saw?', 'Do geese see God?', 'Race fast, safe car', 'Able was I ere I saw Elba', 'Rats live on no evil star', 'A man a plan a Canal Panama' ];
 
-  }
+  //     let random = Math.floor(Math.random * array.length);
+
+  //     this.setState({ words: random });
+
+  // }
+
+  // getRandom = () => {
+
+  //   setInterval(this.createPhrase(), 3000)
+  // }
   
 
   render() {
     return (
-      <div></div>
+      
+      <div className="animation">
+        <span className="phrase">anna</span>
+        <span className="phrase">madam</span>
+        <span className="phrase">10201</span>
+        <span className="phrase">Was it a cat I saw?</span>
+        <span className="phrase">Do geese see God?</span>
+        <span className="phrase">Race fast, safe car</span>
+        <span className="phrase">Able was I ere I saw Elba</span>
+        <span className="phrase">Rats live on no evil star</span>
+        <span className="phrase">A man a plan a Canal Panama</span>
+      </div>
     )
   }
 }
